e8663cbbb904cb885b3004981c2f83756a0adff4
2471974 <24.7.1974@gmail.com>
2020-07-19 16:35:36 -0600

    Update

---
+++ src/main/java/guru/spring/mvcrest/MvcRestApplication.java
@@ -0,0 +1,12 @@
+package guru.spring.mvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRestApplication.class, args);
+	}
+}
---
+++ src/main/java/guru/spring/mvcrest/services/CategoryService.java
@@ -0,0 +1,13 @@
+package guru.spring.mvcrest.services;
+
+
+import guru.spring.mvcrest.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
